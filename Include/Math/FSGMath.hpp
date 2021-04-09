63bbf95fb48de6ba291b39887119c04f194098c0
Lucas Ritter <me@lucasritter.design>
2021-04-09 23:26:29 +0200

    📝 Add docs to math functions

--- Include/Math/FSGMath.hpp
+++ Include/Math/FSGMath.hpp
@@ -1,8 +1,20 @@
 #include <cmath>
 #include "Primitives.hpp"
 
+/**
+ * @param t_x Number.
+ * @return The square root of @b t_x.
+ */
 f64 FSGSqrt(const f32 &t_x);
 
+/**
+ * @param t_x Number.
+ * @return The next integer that's equal or greater than @b t_x.
+ */
 f32 FSGCeil(const f32 &t_x);
 
+/**
+ * @param t_x Number.
+ * @return The next integer that's equal or less than @b t_x.
+ */
 f32 FSGFloor(const f32 &t_x);
