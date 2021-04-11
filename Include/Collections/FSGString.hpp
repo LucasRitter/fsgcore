d7944a7280598c999e7c8756121b0b4640e06ec9
Lucas Ritter <me@lucasritter.design>
2021-04-11 14:51:09 +0200

    ✨ More CString functions

--- Include/Collections/FSGString.hpp
+++ Include/Collections/FSGString.hpp
@@ -5,9 +5,9 @@
 class CString {
 protected:
     bool m_static;
-    char * m_buffer;
+    string m_buffer;
+    u32 m_capacity;
     u32 m_length;
-
     CString();
 
 public:
@@ -43,7 +43,7 @@ public:
      * @param t_offset The offset relative to @b m_buffer where the search will start from.
      * @return The position of the given @b t_string. Otherwise @em -1, if @b t_string can't be found.
      */
-    i32 Find(static_string t_string, i32 t_offset);
+    i32 Find(static_string t_string, i32 t_offset) const;
 
     /**
      * Finds the index where a given @b t_string can be found.
@@ -51,7 +51,13 @@ public:
      * @param t_offset The offset relative to @b m_buffer where the search will start from.
      * @return The position of the given @b t_string. Otherwise @em -1, if @b t_string can't be found.
      */
-    i32 Find(string t_string, i32 t_offset);
+    i32 Find(string t_string, i32 t_offset) const;
+
+    // Todo: Write documentation for CString::ReverseFind
+    i32 ReverseFind(static_string t_string, i32 t_offset) const;
+
+    // Todo: Write documentation for CString::ReverseFind
+    i32 ReverseFind(character t_char, i32 t_offset) const;
 
     // Todo: Write documentation for CString::Remove
     // a1 seems to be completely irrelevant.
@@ -64,16 +70,59 @@ public:
      */
     void Replace(static_string t_find, static_string t_replacement);
 
+    /**
+     * Finds @b t_find in @b m_buffer and replaces it with @t_replacement.
+     * @param t_find The @b character to be found.
+     * @param t_replacement The character to replace @b t_find with.
+     */
+    void Replace(character t_find, character t_replacement);
+
+    // Todo: Write documentation for CString::ReplaceSingleChar
+    void ReplaceSingleChar(character t_replacement, u32 t_offset);
+
+    // Todo: Write documentation for CString::Reverse
+    void Reverse();
+
+    /**
+     * Makes all characters in @b m_buffer uppercase.
+     */
+    void ToUpper();
+
+    /**
+     * Makes all characters in @b m_buffer lowercase.
+     */
+    void ToLower();
+
+    // Todo: Write documentation for CString::TrimLeft
+    void TrimLeft();
+
+    // Todo: Write documentation for CString::TrimRight
+    void TrimRight();
+
+    // Todo: Write documentation for CString::operator static_string()
+    operator static_string() const;
+
+    // Todo: Write documentation for CString::operator==()
+    i32 operator==(static_string t_string) const;
+
+    // Todo: Write documentation for CString::operator!=()
+    i32 operator!=(static_string t_string) const;
+
+
     // Todo: Write documentation for virtual functions.
     virtual void Append(const class CString &t_string) = 0;
     virtual void Append(static_string t_string) = 0;
     virtual void Format(static_string t_format, ...) = 0;
     virtual void Insert(static_string t_string, u32 t_offset) = 0;
     virtual void Insert(const class CString &t_string, u32 t_offset) = 0;
-    virtual void Insert(char, u32 t_offset) = 0;
+    virtual void Insert(character, u32 t_offset) = 0;
 
     // Todo: Implement CString static methods.
     static void StringCopy(string t_target, u32 t_targetCapacity, static_string t_source);
+    static void StringCopyLength(string t_target, u32 t_targetCapacity, static_string t_source, u32 t_sourceLength);
     static void FormatString(string t_target, u32 t_targetCapacity, static_string t_source, ...);
     static void FormatStringLocalised(string t_target, u32 t_targetCapacity, static_string t_source, ...);
+
+    // Todo: Find proper parameter names.
+    static void StringCat(string, u32, static_string);
 };
--- Source/Collections/Generic/FSGString.cpp
+++ Source/Collections/Generic/FSGString.cpp
@@ -6,6 +6,7 @@
 CString::CString() {
     m_static = false;
     m_buffer = nullptr;
+    m_capacity = 0;
     m_length = 0;
 }
 
@@ -50,11 +51,11 @@ void CString::Delete(u32 t_index, u32 t_count) {
     this->m_buffer[newLength] = 0;
 }
 
-i32 CString::Find(static_string t_string, i32 t_offset) {
+i32 CString::Find(static_string t_string, i32 t_offset) const {
     FSG_ASSERT(t_string != nullptr, "Supplied string pointer was NULL.");
 
     auto stringLength = strlen(t_string);
-    auto remaining = m_length - stringLength + 1;
+    auto remaining = this->m_length - stringLength + 1;
 
     auto foundIndex = -1;
 
@@ -75,11 +76,11 @@ i32 CString::Find(static_string t_string, i32 t_offset) {
     return foundIndex;
 }
 
-i32 CString::Find(string t_string, i32 t_offset) {
+i32 CString::Find(string t_string, i32 t_offset) const {
     FSG_ASSERT(t_string != nullptr, "Supplied string pointer was NULL.");
 
     auto stringLength = strlen(t_string);
-    auto remaining = m_length - stringLength - 1;
+    auto remaining = this->m_length - stringLength - 1;
 
     auto foundIndex = -1;
 
@@ -98,6 +99,71 @@ i32 CString::Find(string t_string, i32 t_offset) {
     }
 
     return foundIndex;
+}
+
+i32 CString::ReverseFind(static_string t_string, i32 t_offset) const {
+    // Fixme: Implement CString::ReverseFind()
+    FSG_ASSERT(t_string != nullptr, "Supplied string pointer was NULL");
+
+    auto searchLength = strlen(t_string);
+
+    auto indexOfLastBufferCharacter = this->m_length - 1;
+    auto indexOfLastSearchCharacter = searchLength - 1;
+
+    auto result = -1;
+
+    if (indexOfLastSearchCharacter && indexOfLastSearchCharacter < this->m_length) {
+        if (t_offset < indexOfLastSearchCharacter - 1) {
+            t_offset = indexOfLastSearchCharacter - 1;
+        }
+
+        if (t_offset < indexOfLastBufferCharacter) {
+            auto v15 = indexOfLastBufferCharacter - t_offset;
+
+            do {
+                if (result != -1) {
+                    break;
+                }
+
+                if (*t_string == this->m_buffer[indexOfLastBufferCharacter - t_offset]) {
+                    result = v15;
+                    for (auto i = 1; result != -1; i++) {
+                        if (i >= indexOfLastSearchCharacter) {
+                            break;
+                        }
+                        if (this->m_buffer[i + result] != t_string[i]) {
+                            result = -1;
+                        }
+                    }
+                }
+                t_offset++;
+                v15++;
+            } while (t_offset < indexOfLastBufferCharacter);
+        }
+    }
+
+    return result;
+}
+
+i32 CString::ReverseFind(character t_char, i32 t_offset) const {
+    // For some reason, this function is also implemented differently
+    // to the other functions in the CString class. Outdated?
+
+    if (!this->m_length) {
+        return -1;
+    }
+
+    FSG_ASSERT(t_offset >= 0 && t_offset < this->m_length, "Start offset is outside the bounds of the string.");
+
+    // Custom implementation starting here.
+    // Original was not fun.
+    for (auto i = this->m_length - 1; i >= t_offset; i--) {
+        if (this->m_buffer[i] == t_char) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 void CString::Remove(char a1) {
@@ -125,4 +191,59 @@ void CString::Replace(static_string t_find, static_string t_replacement) {
     }
 }
 
+void CString::Replace(character t_find, character t_replacement) {
+    FSG_ASSERT(!this->m_static, "Statically allocated strings cannot be modified at run-time.");
 
+    for (auto i = 0; i < this->m_length; i++) {
+        if (this->m_buffer[i] == t_find) {
+            this->m_buffer[i] = t_replacement;
+        }
+    }
+}
+
+void CString::ReplaceSingleChar(character t_replacement, u32 t_offset) {
+    FSG_ASSERT(!this->m_static, "Statically allocated strings cannot be modified at run-time.");
+    FSG_ASSERT(t_offset < this->m_length, "Offset is outside string bounds.");
+
+    this->m_buffer[t_offset] = t_replacement;
+}
+
+void CString::Reverse() {
+    FSG_ASSERT(!this->m_static, "Statically allocated strings cannot be modified at run-time.");
+
+    auto steps = this->m_length >> 1;
+    if (steps) {
+        auto i = 0;
+        auto j = this->m_length - 1;
+
+        do {
+            auto temp = this->m_buffer[i];
+            this->m_buffer[i] = m_buffer[j];
+            this->m_buffer[j] = temp;
+
+            i++;
+            j--;
+            steps--;
+        } while (steps);
+    }
+}
+
+void CString::ToUpper() {
+    FSG_ASSERT(!this->m_static, "Statically allocated strings cannot be modified at run-time.");
+
+    for (auto i = 0; i < this->m_length; i++) {
+        if (this->m_buffer[i] >= 'a' && this->m_buffer[i] <= 'z') {
+            this->m_buffer[i] -= 0x20;
+        }
+    }
+}
+
+void CString::ToLower() {
+    FSG_ASSERT(!this->m_static, "Statically allocated strings cannot be modified at run-time.");
+
+    for (auto i = 0; i < this->m_length; i++) {
+        if (this->m_buffer[i] >= 'A' && this->m_buffer[i] <= 'Z') {
+            this->m_buffer[i] += 0x20;
+        }
+    }
+}
