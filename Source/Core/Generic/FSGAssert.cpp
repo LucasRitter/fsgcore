99a5c7671693636ea6d731748f78ac883de210b7
Lucas Ritter <me@lucasritter.design>
2021-04-11 19:32:40 +0200

    🐛 Added temporary fix for CLion Debugging on Windows

--- Source/Core/Generic/FSGAssert.cpp
+++ Source/Core/Generic/FSGAssert.cpp
@@ -12,15 +12,20 @@ void FSGAssert(bool t_truthy, static_string t_function, static_string t_fileName
     printf_s("ERROR:\n  %s\n  %s\n  %s:%d\n  ", t_expression, t_function, t_fileName, t_line);
 
     if (t_message == nullptr) {
-        return;
+        std::abort();
     }
 
     va_list args;
     va_start(args, t_message);
-    vprintf_s(t_message, args);
-    va_end(args);
 
-    printf("\n");
+    auto buffer = reinterpret_cast<string>(malloc(0x800));
+    vsprintf_s(buffer, 0x800, t_message, args);
+
+    // Temporary fix for an issue with stdout buffering and CLion on Windows.
+    setbuf(stdout, nullptr);
+    printf("%s\n", buffer);
+
+    va_end(args);
 
     std::abort();
 }
