87f66410bec34f7219c16f70c1de891695512bed
Lucas Ritter <me@lucasritter.design>
2021-06-23 19:06:12 +0200

    ✨ Added NextMultiple math function

--- Include/Math/FSGMath.hpp
+++ Include/Math/FSGMath.hpp
@@ -20,3 +20,10 @@ f32 FSGCeil(const f32& x);
  * @return The next integer that's equal or less than @b x.
  */
 f32 FSGFloor(const f32& x);
+
+/**
+ * @param value Number.
+ * @param multiple The multiple to round up to.
+ * @return The next integer that's a multiple of @b multiple.
+ */
+u32 FSGNextMultiple(u32 value, u32 multiple);
--- Source/Math/Generic/FSGMath.cpp
+++ Source/Math/Generic/FSGMath.cpp
@@ -14,3 +14,14 @@ f32 FSGFloor(const f32& x)
 {
     return floor(x);
 }
+
+u32 FSGNextMultiple(u32 value, u32 multiple) {
+    if (multiple == 0)
+        return value;
+
+    auto remainder = value % multiple;
+    if (remainder == 0)
+        return value;
+
+    return value + multiple - remainder;
+}
